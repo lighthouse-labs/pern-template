8ebaadbd32dd0302a420416885c794da1647892e
Manuel Casanova <manucasanova@hotmail.com>
2022-09-07 10:46:16 -0700

    Add missing process.env

--- server/db.js
+++ server/db.js
@@ -4,8 +4,8 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
 user: process.env.DB_USER,
 host: 'localhost',
-database: REACT_APP_DB_NAME,
-password: REACT_APP_DB_PASSWORD,
+database: process.env.REACT_APP_DB_NAME,
+password: process.env.REACT_APP_DB_PASSWORD,
 port: 5432
 })
 module.exports = pool;
