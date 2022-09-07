0bbbcdcbfbea10e5f4bccd84a450cd25dff978e4
Manuel Casanova <manucasanova@hotmail.com>
2022-09-07 08:39:17 -0700

    Set up server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8001;
+const cors = require('cors');
+app.use(cors());
+app.use(express.json()); //req.body
+
+
+
+
+app.listen(port, () => {
+console.log(`The app is running on port ${port}.`);
+});
