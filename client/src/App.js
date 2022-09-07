f9e4e09ac283a18363ace32ff5c83ac0fc34db3d
Manuel Casanova <manucasanova@hotmail.com>
2022-09-07 09:18:18 -0700

    Initialize react app and render a component

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+//CSS
+import './App.css';
+
+//Components
+import List from "./components/List"
+
+
+function App() {
+  return (
+    <div className="App">
+      <List />
+    </div>
+  );
+}
+
+export default App;
