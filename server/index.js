a06dc4d9d9edbb6c24815c6eb767df6d2d9c61c2
Manuel Casanova <manucasanova@hotmail.com>
2022-09-07 10:46:43 -0700

    Add query to read items from db

--- server/index.js
+++ server/index.js
@@ -4,7 +4,22 @@ const port = process.env.PORT || 8001;
 const cors = require('cors');
 app.use(cors());
 app.use(express.json()); //req.body
+const pool = require("./db")
 
+//Routes and queries
+
+//Get all items
+
+app.get("/items", async (req, res) => {
+  try {
+    const getAllItems = await pool.query(
+    'SELECT * FROM users'
+    );
+    res.json(getAllItems.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+});
 
 
 app.listen(port, () => {
